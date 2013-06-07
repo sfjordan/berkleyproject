cf2fd56f2ad8283054cdd8f40731f420f28c22d2
Sam Jordan <samuelfjordan@gmail.com>
2013-06-07 14:02:20 -0700

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Berkleyproject::Application.config.secret_token = '91b40fddd6ff472838b4beac25408129cf06da18e42f6fa04bd337fa09257b35c738d81e8e739ad574ca681b6f635ed9c2a27773c5124b3e2d42c9b338a77893'
